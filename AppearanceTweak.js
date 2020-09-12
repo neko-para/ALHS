a7842ecad8fe369b5810969c5cf4df9fb7a531da
nekosu <liao.hy@outlook.com>
2020-09-12 23:30:30 +0800

    fix error

--- AppearanceTweak.js
+++ AppearanceTweak.js
@@ -11,7 +11,7 @@
 	let root = $(`<div class="ALHS_AM_CONFIG_ROW"><span style="margin-right: 5px">字体:</span><input type="text" /></div>`);
 	let input = $('input', root);
 	input.val(font);
-	input.input(function () {
+	input.on('input', function () {
 		font = input.val().replace(/^\s*/, '').replace(/\s*$/, '');
 		updateStyle();
 	});
