1e567b989fd8db966038308653bd267bb7fca6b9
nekosu <liao.hy@outlook.com>
2020-09-13 17:36:59 +0800

    add new mirror support. fix tweak active url.

--- ALHSAddonManagerLoader.js
+++ ALHSAddonManagerLoader.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         艾利浩斯图书馆插件管理器
 // @namespace    https://ailihaosi.xyz/
-// @version      0.1.0
+// @version      0.1.1
 // @description  为图书馆的插件提供方便的设置和更新功能
 // @author       nekosu
 // @include      https://ailihaosi.xyz/**
+// @include      https://alhs.live/**
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @require      https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
